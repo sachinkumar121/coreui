fe0ad4bebb6cecdb7a667b7b6f135dfa30a961a4
sachinkumar121 <skarora7894@gmail.com>
2025-05-30 06:39:14 +0530

    create mobile app using capacitor.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.test.app',
+  appName: '@silverskysoft/core-ui',
+  webDir: 'dist'
+};
+
+export default config;
